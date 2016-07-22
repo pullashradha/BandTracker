f6f801f6c7f14dacb840d82b3ad90ba68ea9a313
Shradha Pulla <pullashradha@gmail.com>
2016-07-21 19:45:54 -0700

    Create all tests in BandTest.cs

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -12,9 +12,73 @@ namespace BandTracker
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
     }
+    [Fact]
+    public void Test_Empty_EmptyDatabase()
+    {
+      int resultValue = Band.GetAll().Count;
+      Assert.Equal(0, resultValue);
+    }
+    [Fact]
+    public void Test_Equals_EntriesMatch()
+    {
+      Band firstBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      Band secondBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      Assert.Equal(firstBand, secondBand);
+    }
+    [Fact]
+    public void Test_Save_SavesBandToDatabase()
+    {
+      Band newBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      newBand.Save();
+      List<Band> testList = new List<Band> {newBand};
+      List<Band> resultList = Band.GetAll();
+      Assert.Equal(testList, resultList);
+    }
+    [Fact]
+    public void Test_AddVenue_SavesVenueToBand()
+    {
+      Band newBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      newBand.Save();
+      Venue newVenue = new Venue ("Crossroads MegaStadium", "101 SW Washington St.", "Seattle", "Washington", "97206", "555-555-5555", "www.crossroadsstadium.com", new DateTime(2020, 3, 25));
+      newVenue.Save();
+      newBand.AddVenue(newVenue);
+      List<Venue> testVenueList = new List<Venue> {newVenue};
+      List<Venue> resultVenueList = newVenue.GetVenues();
+      Assert.Equals(testVenueList, resultVenueList);
+    }
+    [Fact]
+    public void Test_Find_ReturnsBandById()
+    {
+      Band newBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      newBand.Save();
+      Band foundBand = Band.Find(newBand.GetId());
+      Assert.Equal(newBand, foundBand);
+    }
+    [Fact]
+    public void Test_Update_UpdatesBandInformation()
+    {
+      Band newBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      newBand.Save();
+      newBand.SetDescription("The band is currently touring the US.");
+      newBand.Update();
+      Band updatedBand = Band.Find(newBand.GetId());
+      Assert.Equal(newBand.GetDescription(), updatedBand.GetDescription());
+    }
+    [Fact]
+    public void Test_DeleteOne_DeletesOneBand()
+    {
+      Band firstBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      Band secondBand = new Band ("One Ok Rock", "Pop/Rock", "Band from Japan, currently touring the US.", "www.oneokrock.com");
+      firstBand.Save();
+      secondBand.Save();
+      List<Band> testList = new List<Band> {secondBand};
+      firstBand.DeleteOne();
+      List<Band> resultList = Band.GetAll();
+      Assert.Equal(testList, resultList);
+    }
     public void Dispose()
     {
-      // Band.DeleteAll();
+      Band.DeleteAll();
     }
   }
 }
