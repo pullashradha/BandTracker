6bd10cbc84a13c0c78bf167a7fb3ea5d97d57624
Shradha Pulla <pullashradha@gmail.com>
2016-07-22 09:54:22 -0600

    Add input form for adding a new venue to venue.cshtml, connect page in HomeModule.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,21 @@ namespace BandTracker
       Get ["/"] = _ => View ["index.cshtml"];
       Get ["/bands"] = _ => View ["bands.cshtml", Band.GetAll()];
       Get ["/venues"] = _ => View ["venues.cshtml", Venue.GetAll()];
+      Post ["/venues/new"] = _ => {
+        Venues newVenue = new Venue
+        (
+          Request.Form ["venue-name"],
+          Request.Form ["venue-street-address"],
+          Request.Form ["venue-city"],
+          Request.Form ["venue-state"],
+          Request.Form ["venue-zipcode"],
+          Request.Form ["venue-phone-number"],
+          Request.Form ["venue-website"],
+          Request.Form ["venue-event-date"]
+        );
+        newVenue.Save();
+        return View ["/venues.cshtml", Venue.GetAll()];
+      };
       // Post ["/bands/{id}/{name}"] = parameters => {
       //   Band selectedBand = Band.Find(parameters.id);
       //   selectedBand.Save();
