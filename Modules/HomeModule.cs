c773bcb9fec8fdaeee5bbba46f61a96a1ed6eee6
Shradha Pulla <pullashradha@gmail.com>
2016-07-22 11:15:02 -0600

    Add update & delete options in band.cshtml & venue.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -24,6 +24,22 @@ namespace BandTracker
         Band selectedBand = Band.Find(parameters.id);
         return View ["band.cshtml", selectedBand];
       };
+      Post ["/bands/{id}/{name}/updated"] = parameters => {
+        Band selectedBand = Band.Find(parameters.id);
+        Band updatedBand = selectedBand
+        (
+          Request.Form ["update-band-name"],
+          Request.Form ["update-band-music-genre"],
+          Request.Form ["update-band-description"],
+          Request.Form ["update-band-website"]
+        );
+        updatedBand.Update();
+        return View ["band.cshtml", updatedBand];
+      };
+      Post ["/bands/deleted"] = _ => {
+        Band.DeleteAll();
+        return View ["bands.cshtml", Band.GetAll()];
+      };
       Post ["/venues/new"] = _ => {
         Venue newVenue = new Venue
         (
@@ -43,6 +59,27 @@ namespace BandTracker
       Post ["/venues/{id}/{name}"] = parameters => {
         Venue selectedVenue = Venue.Find(parameters.id);
         return View ["venue.cshtml", selectedVenue];
+      };
+      Post ["/venues/{id}/{name}/updated"] = parameters => {
+        Venue selectedVenue = Venue.Find(parameters.id);
+        Venue updatedVenue = selectedVenue
+        (
+          Request.Form ["update-venue-id"],
+          Request.Form ["update-venue-name"],
+          Request.Form ["update-venue-street-address"],
+          Request.Form ["update-venue-city"],
+          Request.Form ["update-venue-state"],
+          Request.Form ["update-venue-zipcode"],
+          Request.Form ["update-venue-phone-number"],
+          Request.Form ["update-venue-website"],
+          Request.Form ["update-venue-event-date"]
+        );
+        updatedVenue.Update();
+        return View ["venue.cshtml", updatedVenue];
+      };
+      Post ["/venues/deleted"] = _ => {
+        Venue.DeleteAll();
+        return View ["venues.cshtml", Venue.GetAll()];
       };
     }
   }
