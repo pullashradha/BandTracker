892180709aff8c6760ac4d447630532e24604e14
Shradha Pulla <pullashradha@gmail.com>
2016-07-22 09:30:57 -0600

    Fix connection problem in HomeModule by changing /bands & /venues routed from Post to Get. ERROR FOUND: There are 19 instances of One Ok Rock in the bands table...

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -7,18 +7,18 @@ namespace BandTracker
     public HomeModule()
     {
       Get ["/"] = _ => View ["index.cshtml"];
-      Post ["/bands"] = _ => View ["bands.cshtml", Band.GetAll()];
-      Post ["/venues"] = _ => View ["venues.cshtml", Venue.GetAll()];
-      Post ["/bands/{id}/{name}"] = parameters => {
-        Band selectedBand = Band.Find(parameters.id);
-        selectedBand.Save();
-        return View ["band.cshtml", selectedBand];
-      };
-      Post ["/venues/{id}/{name}"] = parameters => {
-        Venue selectedVenue = Venue.Find(parameters.id);
-        selectedVenue.Save();
-        return View ["venue.cshtml", selectedVenue];
-      };
+      Get ["/bands"] = _ => View ["bands.cshtml", Band.GetAll()];
+      Get ["/venues"] = _ => View ["venues.cshtml", Venue.GetAll()];
+      // Post ["/bands/{id}/{name}"] = parameters => {
+      //   Band selectedBand = Band.Find(parameters.id);
+      //   selectedBand.Save();
+      //   return View ["band.cshtml", selectedBand];
+      // };
+      // Post ["/venues/{id}/{name}"] = parameters => {
+      //   Venue selectedVenue = Venue.Find(parameters.id);
+      //   selectedVenue.Save();
+      //   return View ["venue.cshtml", selectedVenue];
+      // };
     }
   }
 }
