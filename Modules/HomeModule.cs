f5d9e10dc35b9b1bb27eaf15ce839b6a8b29dd8b
Shradha Pulla <pullashradha@gmail.com>
2016-07-22 10:40:50 -0600

    Slight html changes, having trouble with viewing a single band's page even though information is being stored in the database

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,32 +17,31 @@ namespace BandTracker
           Request.Form ["band-description"],
           Request.Form ["band-website"]
         );
+        newBand.Save();
         return View ["bands.cshtml", Band.GetAll()];
       };
-      Post ["/venues/new"] = _ => {
-        Venue newVenue = new Venue
-        (
-          Request.Form ["venue-id"],
-          Request.Form ["venue-name"],
-          Request.Form ["venue-street-address"],
-          Request.Form ["venue-city"],
-          Request.Form ["venue-state"],
-          Request.Form ["venue-zipcode"],
-          Request.Form ["venue-phone-number"],
-          Request.Form ["venue-website"],
-          Request.Form ["venue-event-date"]
-        );
-        newVenue.Save();
-        return View ["venues.cshtml", Venue.GetAll()];
+      Post ["/bands/{id}/{name}"] = parameters => {
+        Band selectedBand = Band.Find(parameters.id);
+        return View ["band.cshtml", selectedBand];
       };
-      // Post ["/bands/{id}/{name}"] = parameters => {
-      //   Band selectedBand = Band.Find(parameters.id);
-      //   selectedBand.Save();
-      //   return View ["band.cshtml", selectedBand];
+      // Post ["/venues/new"] = _ => {
+      //   Venue newVenue = new Venue
+      //   (
+      //     Request.Form ["venue-id"],
+      //     Request.Form ["venue-name"],
+      //     Request.Form ["venue-street-address"],
+      //     Request.Form ["venue-city"],
+      //     Request.Form ["venue-state"],
+      //     Request.Form ["venue-zipcode"],
+      //     Request.Form ["venue-phone-number"],
+      //     Request.Form ["venue-website"],
+      //     Request.Form ["venue-event-date"]
+      //   );
+      //   newVenue.Save();
+      //   return View ["venues.cshtml", Venue.GetAll()];
       // };
       // Post ["/venues/{id}/{name}"] = parameters => {
       //   Venue selectedVenue = Venue.Find(parameters.id);
-      //   selectedVenue.Save();
       //   return View ["venue.cshtml", selectedVenue];
       // };
     }
