e605af57c5b08f839db3e8683a9bfe698a7106bd
Shradha Pulla <pullashradha@gmail.com>
2016-07-26 19:54:02 -0600

    Fix errors in HomeModule.cs to make adding a band to a venue or vice versa work, ERROR FOUND: browser erro messages for the Update Band & AddBand/AddVenue pop up but the program still runs & stores the appropriate data...

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -39,20 +39,9 @@ namespace BandTracker
         selectedBand.Update();
         return View ["band.cshtml", selectedBand];
       };
-      Post ["/bands/{id}/{name}/new"] = parameters => {
+      Post ["/bands/{id}/{name}/add_venue"] = parameters => {
         Band selectedBand = Band.Find(parameters.id);
-        Venue newVenue = new Venue
-        (
-          Request.Form ["venue-name"],
-          Request.Form ["venue-street-address"],
-          Request.Form ["venue-city"],
-          Request.Form ["venue-state"],
-          Request.Form ["venue-zipcode"],
-          Request.Form ["venue-phone-number"],
-          Request.Form ["venue-website"],
-          Request.Form ["venue-event-date"]
-        );
-        newVenue.Save();
+        Venue newVenue = Venue.Find(Request.Form ["venue-id"]);
         selectedBand.AddVenue(newVenue);
         return View ["band.cshtml", selectedBand];
       };
@@ -102,16 +91,9 @@ namespace BandTracker
         selectedVenue.Update();
         return View ["venue.cshtml", selectedVenue];
       };
-      Post ["/venues/{id}/{name}/new"] = parameters => {
+      Post ["/venues/{id}/{name}/add_band"] = parameters => {
         Venue selectedVenue = Venue.Find(parameters.id);
-        Band newBand = new Band
-        (
-          Request.Form ["band-name"],
-          Request.Form ["band-music-genre"],
-          Request.Form ["band-description"],
-          Request.Form ["band-website"]
-        );
-        newBand.Save();
+        Band newBand = Band.Find(Request.Form ["band-id"]);
         selectedVenue.AddBand(newBand);
         return View ["venue.cshtml", selectedVenue];
       };
